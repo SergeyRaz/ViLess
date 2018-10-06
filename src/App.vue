1cbedef26f43203c42364e413371311d9c3046fb
SergeyRazzhivin <razjiwin2010@yandex.ru>
2018-10-06 19:54:52 +0300

    dgjftgh

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <ViewVideo></ViewVideo>
+  </div>
+</template>
+
+<script>
+import ViewVideo from "./components/ViewVideo.vue";
+export default {
+  components: {
+    ViewVideo
+  }
+};
+</script>
+
+<style lang="scss">
+@import "src/scss/global.scss";
+</style>
---
+++ src/components/ViewVideo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <video class="video" controls>
+      <source src="src/video/vue7.mp4" type = "video/mp4">
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
